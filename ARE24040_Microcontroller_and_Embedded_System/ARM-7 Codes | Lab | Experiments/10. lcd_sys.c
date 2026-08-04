85cefe4f77c3fd3620383d64428ee58c7ebb02a8
Sabari Poornesh S <geetaclass425@gmail.com>
2026-08-04 15:25:41 +0530

    Update 10. lcd_sys.c

--- ARE24040_Microcontroller_and_Embedded_System/ARM-7 Codes | Lab | Experiments/10. lcd_sys.c
+++ ARE24040_Microcontroller_and_Embedded_System/ARM-7 Codes | Lab | Experiments/10. lcd_sys.c
@@ -14,7 +14,7 @@ void delay(unsigned int);
 
 const unsigned char cmd[4] = {0x28, 0x0C, 0x06, 0x01};
 unsigned char msg[]  = {" Sharbesh"};
-unsigned char msg1[] = {" jk "};
+unsigned char msg1[] = {" Loves "};
 
 int main(void)
 {
